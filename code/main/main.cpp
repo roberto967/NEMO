1c78c7ddd7e7fdd5c31292129652240d30f89585
Roberto <robertoo.jof@gmail.com>
2024-05-27 03:13:55 -0300

    aparteid de inicialização do servo

--- code/main/main.cpp
+++ code/main/main.cpp
@@ -2,16 +2,19 @@
 
 using namespace std;
 
+// Pins do servo que abre a comporta de comida
 #define SERVO_FG1_GPIO GPIO_NUM_26
 #define SERVO_FG1_CHANNEL LEDC_CHANNEL_0
 
+// Pins do sensor de proximidade
 #define MAX_DISTANCE_CM 500 // 5m max
 #define ECHO_GPIO GPIO_NUM_5
 #define TRIGGER_GPIO GPIO_NUM_18
 
-// Função para abrir e fechar comporta de comida
-void open_foodgate() {
-  servo_config_t servo_foodgate_1 = {
+servo_config_t servo_foodgate_1;
+
+void setup_servo(void) {
+  servo_foodgate_1 = {
       .max_angle = 180,     // Ângulo máximo do servo
       .min_width_us = 500,  // Largura de pulso mínima para o ângulo mínimo
       .max_width_us = 2500, // Largura de pulso máxima para o ângulo máximo
@@ -32,15 +35,24 @@ void open_foodgate() {
   };
 
   // Inicialização do servo da comporta de comida
-  iot_servo_init(LEDC_HIGH_SPEED_MODE, &servo_foodgate_1);
-
-  iot_servo_write_angle(LEDC_HIGH_SPEED_MODE, 0, 180.0f);
-
-  // dai espera o tempo que a comida cai pra poder fechar, vai precisar
-  // sincronizar com o sensor de peso
-
-  // Desinicializar o servo da comporta de comida
-  iot_servo_deinit(LEDC_HIGH_SPEED_MODE);
+  ESP_ERROR_CHECK(iot_servo_init(LEDC_HIGH_SPEED_MODE, &servo_foodgate_1));
 }
 
-extern "C" void app_main(void) {}
+// Função para abrir e fechar comporta de comida
+void oc_foodgate(void) {
+  // Abre a comporta
+  ESP_ERROR_CHECK(
+      iot_servo_write_angle(LEDC_HIGH_SPEED_MODE, SERVO_FG1_CHANNEL, 180.0f));
+
+  // Espera o tempo que a comida cai pra poder fechar, precisa
+  // sincronizar com o sensor de peso
+
+  // Fecha a comporta
+  ESP_ERROR_CHECK(
+      iot_servo_write_angle(LEDC_HIGH_SPEED_MODE, SERVO_FG1_CHANNEL, 0.0f));
+}
+
+extern "C" void app_main(void) {
+  // Inicializar o servo da comporta de comida
+  setup_servo();
+}
--- code/main/main.h
+++ code/main/main.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "driver/gpio.h"
+#include "esp_log.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
